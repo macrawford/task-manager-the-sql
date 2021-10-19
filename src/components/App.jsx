f7b70f9a9b4907650eafa47f4cb23b526c2137a7
Matthew Crawford <matthewcrawford@Matthews-Air.home>
2021-10-19 11:44:50 -0700

    Started feature branch for adding, deleting and marking complete to-dos

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,43 @@ import React, { useState } from 'react';
 
 function App() {
     const [input, setInput] = React.useState('');
+    const [todos, setTodos] = React.useState([]);
+    const handleChange = (e) => {
+        setInput(e.target.value);
+        console.log('input: ', input)
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        var sliced = todos.slice();
+        sliced.push(input);
+        setTodos(sliced);
+    }
     return (
         <div>
-            React working!
+            <div>
+                To-Do List
+            </div>
+            <form>
+                <input onChange={handleChange} placeholder={'Add to-do'}></input>
+                <button onClick={handleSubmit}>Submit</button>
+            </form>
+            <div>
+                {todos.map((todo, index) => {
+                    return(
+                        <div key={index}>
+                            <div>
+                                {todo}
+                            </div>
+                            <button>
+                                Mark completed
+                            </button>
+                            <button>
+                                Delete
+                            </button>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
