c8507723ee555dcc050104aadd1a91a6708cfaa4
Matthew Crawford <matthewcrawford@Matthews-MacBook-Air.local>
2021-10-20 17:18:17 -0700

    Set up MySQL files and routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,31 @@
-const express = require('express')
+const express = require('express');
+const db = require('../database/config/db');
 
 const app = express();
 
 const PORT = 3000 || process.env.PORT;
 
 app.use(express.static('public'))
+
+app.get("/api/get", (req, res) => {
+    db.query("SELECT * FROM todos", (err, results) => {
+        if (err) {
+            console.log('error: ', err)
+        }
+        res.send(results)
+    })
+})
+
+app.post("api/create", (req, res) => {
+    const text = req.body.text;
+    const completed = req.body.completed;
+    db.query("INSERT INTO posts (text, completed) VALUES (?, ?)", [text, completed], (err, results) => {
+        if (err) {
+            console.log('error: ', err)
+        }
+        console.log(results)
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`App running on localhost:${PORT}`)
